7e8a825334333907375fb3589390ee103472bff7
DanGr52 <axadovd05@mail.ru>
2026-09-08 16:29:00 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,7 @@
+﻿double celsius = 23.5;
+const int a = 32;
+double farengeut = (celsius * 9) / 5 + a;
+double kelvin = celsius + 273.15;
+Console.WriteLine($"{celsius}C = {farengeut}Ф = {kelvin}К");
+
+
