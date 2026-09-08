6edeea5a8726d1c5dd3ff62ff8665d3609cdc228
DanGr52 <axadovd05@mail.ru>
2026-09-08 16:10:37 +0300

    Lab01 task 1: budget calculation

--- Visitka/Program.cs
+++ Visitka/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine();
 int numberGroup = 251;
 string spekial = "ИСП";
-int kurse = 2;
+const int kurse = 2;
 int modul1 = 30;
 int modul2 = 20;
 int modul3 = 25;
@@ -12,3 +12,14 @@ double sred = simester / 3; // среднее количество баллов 
 Console.WriteLine("====Визитная Карточка Гайворонского Александра====");
 Console.WriteLine($"- Номер группы: {numberGroup}  "); Console.WriteLine($"- Специальность: {spekial} "); Console.WriteLine($"- Курс: {kurse}");
 Console.WriteLine($"- Баллы за семестр: {simester} "); Console.WriteLine($"- Среднее количество баллов за модуль: {sred} ");
+
+Console.WriteLine();
+
+int scholarship = 1200000;
+int
+monthlyExpenses = 1100000;
+int MonthsInSemester = 4;
+int ostatok = scholarship - monthlyExpenses;
+int ostatok4 = ostatok * MonthsInSemester;
+Console.WriteLine($"Остаток после трат: {ostatok}");
+Console.WriteLine($"Остаток после трат за семестр: {ostatok4}");
